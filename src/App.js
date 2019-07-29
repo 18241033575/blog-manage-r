436f5b7663482b1283f1d679164a40b11a6a36a1
Jayshi <240638896@qq.com>
2019-07-29 15:50:36 +0800

    start

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import "./static/css/index.css"
-import "//at.alicdn.com/t/font_1305986_nds5bwcw4zn.css"
+// import "//at.alicdn.com/t/font_1305986_nds5bwcw4zn.css"
 import Main from "./components/Main/Main"
 
 function App() {
