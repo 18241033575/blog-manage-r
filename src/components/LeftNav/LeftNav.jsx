c7f80dafd949776b6e7f42cf548ff3b4113b26b2
Jayshi <240638896@qq.com>
2019-07-29 15:50:00 +0800

    start

--- src/components/LeftNav/LeftNav.jsx
+++ src/components/LeftNav/LeftNav.jsx
@@ -8,13 +8,13 @@ class LeftNav  extends Component {
       return(
           <div className={'menu'}>
               <div className="logo">
-                  <img src={require('../../static/img/React.png')} alt=""/><span>China React</span>
+                  <img src={require('../../static/img/React.png')} alt=""/><span>后台管理系统</span>
               </div>
               <div className="aside_nav">
                   <ul>
-                      <li><NavLink exact activeClassName={'nav_active'} to="/">首页</NavLink></li>
+                      <li><NavLink exact activeClassName={'nav_active'} to="/"><i className={'iconfont icon-home'}></i>首页</NavLink></li>
                       <li><NavLink activeClassName={'nav_active'} to="/line">文章管理</NavLink></li>
-                      <li><NavLink activeClassName={'nav_active'} to="circle">评论列表</NavLink></li>
+                      <li><NavLink activeClassName={'nav_active'} to="/circle">评论列表</NavLink></li>
                       {/*<li><NavLink activeClassName={'nav_active'} to="/test/circle">测试页</NavLink></li>*/}
                   </ul>
               </div>
