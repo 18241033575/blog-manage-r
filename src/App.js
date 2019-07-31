044375644add441a7ce4a443f631757166fc5873
Jayshi <240638896@qq.com>
2019-07-31 11:50:50 +0800

    antd

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+// 默认第一组组件
 import Main from "./components/Main/Main"
-
+// 默认第二组样式
 import "./static/css/index.css"
-
+import "antd/dist/antd.css"
+// 剩余图片等 -- 后面不再注释
 function App() {
     return (
         <Main />
